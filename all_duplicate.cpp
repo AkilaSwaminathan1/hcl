7ff6c77ba98ab62337963bf6ce14eb99962c567b
Akila Swaminathan <akilaswaminathan2005@gmail.com>
2026-03-06 09:22:43 +0530

    Find all duplicate elements in an array

---
+++ all_duplicate.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j])
+                cout << arr[i] << " ";
+        }
+    }
+    return 0;
+}
